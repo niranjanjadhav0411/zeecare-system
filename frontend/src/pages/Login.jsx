a632b3928833c26e3820bc2ad54241c39bbc291d
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2025-11-11 19:08:41 +0530

    final commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  // const navigateTo = useNavigate();
+  const navigateTo = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
